f1708f5554a19e4bffaca0c8138f4c3c9a799016
Alin <voia.alin@yahoo.com>
2022-01-04 20:49:52 +0200

    jump
    added double jump and improved jumping feeling

--- MSA_Project/Assets/StoryModeScene/Movement.cs
+++ MSA_Project/Assets/StoryModeScene/Movement.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Movement : MonoBehaviour
 {
 	private Rigidbody2D rb;
-	private float dirX;
-	private float moveSpeed = 10f;
-    private float jumpForce = 400f;
+	private float moveInputHorizontal;
+	public float moveSpeed = 10f;
+    public float jumpForce = 9f;
+    private float fallMultiplier = 2.5f;
+
+    public LayerMask ground; //checks if its the ground layer so that the square can touch it
+    public Transform groundCheck; //object to check if square is touching ground
+    public float groundCheckRadius; //little area used to detect collisions
+    private bool isGrounded;
+
+    public int maxNrJumps;
+    private int nrJumps;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +26,35 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
+    /*void FixedUpdate()
+    {
+
+
+    }*/
+
+
     void Update()
     {
-        dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
-		rb.velocity = new Vector2(dirX, rb.velocity.y);
-        
-        //if (CrossPlatformInputManager.GetButtonDown("Jump") && rb.velocity.y == 0)
-        if (CrossPlatformInputManager.GetButtonDown("Jump"))
-            rb.AddForce(Vector2.up * jumpForce);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
 
+        moveInputHorizontal = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(moveInputHorizontal, rb.velocity.y);
+
+        if (isGrounded){
+            nrJumps = maxNrJumps;
+        }
+
+        //if (CrossPlatformInputManager.GetButtonDown("Jump") && rb.velocity.y == 0)
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && nrJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            nrJumps--;
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            //rb.AddForce(Vector2.up * jumpForce);
+
+        }
+        if (rb.velocity.y < 0)
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
     }
 }
