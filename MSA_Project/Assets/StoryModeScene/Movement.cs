9c9f8adcf2dde568741e11ce5f8a21ef750659f7
Alin <voia.alin@yahoo.com>
2022-01-04 03:00:05 +0200

    camera
    added camera  smooth follow

--- MSA_Project/Assets/StoryModeScene/Movement.cs
+++ MSA_Project/Assets/StoryModeScene/Movement.cs
@@ -6,16 +6,23 @@ public class Movement : MonoBehaviour
 	private Rigidbody2D rb;
 	private float dirX;
 	private float moveSpeed = 10f;
+    private float jumpForce = 400f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //Physics.gravity = new Vector3(0, -50, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        dirX = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed;
-		rb.velocity = new Vector2(dirX, 0f);
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+		rb.velocity = new Vector2(dirX, rb.velocity.y);
+        
+        //if (CrossPlatformInputManager.GetButtonDown("Jump") && rb.velocity.y == 0)
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            rb.AddForce(Vector2.up * jumpForce);
+
     }
 }
