473a2d308ddb959a3ad044bd14b47c21305ee584
Alin <voia.alin@yahoo.com>
2022-01-08 00:34:11 +0200

    level vers2

--- MSA_Project/Assets/StoryModeScene/Movement.cs
+++ MSA_Project/Assets/StoryModeScene/Movement.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour
 {
 	private Rigidbody2D rb;
 	private float moveInputHorizontal;
-	public float moveSpeed = 10f;
+	public float moveSpeed = 6f;
     public float jumpForce = 9f;
     private float fallMultiplier = 2.5f;
 
@@ -17,18 +17,20 @@ public class Movement : MonoBehaviour
     public Transform groundCheck3;
     public Transform groundCheck4;
     public float groundCheckRadius; //little area used to detect collisions
-    public bool isGrounded1;
-    public bool isGrounded2;
-    public bool isGrounded3;
-    public bool isGrounded4;
+    private bool isGrounded1;
+    private bool isGrounded2;
+    private bool isGrounded3;
+    private bool isGrounded4;
 
     public int maxNrJumps;
     private int nrJumps;
 
     SavePlayerPos playerPosData;
 
-    public bool isJumping;
-
+    Vector2 checkpoint1;
+    Vector2 checkpoint2;
+    Vector2 checkpoint3;
+    private int check1 = 0;
 
     private void Awake() {
 
@@ -40,6 +42,10 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        checkpoint1 = new Vector2(0, 0);
+        checkpoint2 = new Vector2(16, -1.3f);
+        checkpoint3 = new Vector2(54, -1.3f);
+        
         //Physics.gravity = new Vector3(0, -50, 0);
     }
 
@@ -54,6 +60,25 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+
+
+        if (transform.position.y < -16 && transform.position.x >= 53.5)
+        {
+            transform.position = checkpoint3;
+            //check1 = 1;
+        }
+        else if (transform.position.y < -16 && transform.position.x >= 16)
+        {
+            transform.position = checkpoint2;
+            //check1 = 1;
+        }
+        else if (transform.position.y < -16)
+        {
+            transform.position = checkpoint1;
+            //check1 = 1;
+        }
+
+
         isGrounded1 = Physics2D.OverlapCircle(groundCheck1.position, groundCheckRadius, ground);
         isGrounded2 = Physics2D.OverlapCircle(groundCheck2.position, groundCheckRadius, ground);
         isGrounded3 = Physics2D.OverlapCircle(groundCheck3.position, groundCheckRadius, ground);
@@ -73,7 +98,7 @@ public class Movement : MonoBehaviour
         {
             rb.velocity = Vector2.up * jumpForce;
             nrJumps--;
-            isJumping = true;
+            
             //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             //rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
             //rb.AddForce(Vector2.up * jumpForce);
